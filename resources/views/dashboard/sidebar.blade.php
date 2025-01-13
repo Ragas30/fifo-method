7b51f6f046ef1e8d87177c8c6d762dcb4ec714d7
Ragas30 <130697528+Ragas30@users.noreply.github.com>
2025-01-13 20:12:51 +0700

    responsive design

--- resources/views/dashboard/sidebar.blade.php
+++ resources/views/dashboard/sidebar.blade.php
@@ -5,7 +5,7 @@
     <span class="block w-6 h-1 bg-white"></span>
 </button>
 
-<aside id="mobileMenu" class="hidden md:block w-[200px] bg-black p-4 justify-between">
+<aside id="mobileMenu" class="hidden md:block md:w-[200px] bg-black p-4">
     <nav class="space-y-1 flex flex-col justify-between h-full">
         <div class="font-bold">
             <a href="{{ route('dashboard') }}"
@@ -18,10 +18,11 @@
             <a href="{{ route('transaksi') }}"
                 class="block py-2 px-4 rounded-md text-white hover:bg-lime-600">Transaksi</a>
             <a href="{{ route('listTransaksi') }}" class="block py-2 px-4 rounded-md text-white hover:bg-lime-600">List
-                Transaksi</a></a>
+                Transaksi</a>
             <a href="{{ route('penjualan') }}"
-                class="block py-2 px-4 rounded-md text-white hover:bg-lime-600">Penjualan</a></a>
+                class="block py-2 px-4 rounded-md text-white hover:bg-lime-600">Penjualan</a>
         </div>
         <a href="{{ route('logout') }}" class="block py-2 px-4 rounded-md text-white hover:bg-lime-600">logout</a>
     </nav>
 </aside>
+
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -32,7 +32,7 @@
 
             <div class="flex items-center justify-center">
                 <div id="background"
-                    class="bg-white w-40 h-40 sm:w-56 sm:h-56 md:w-64 md:h-64 lg:w-72 lg:h-72 rounded-full overflow-hidden shadow-lg">
+                    class="bg-white w-40 h-40 sm:w-56 sm:h-56 md:w-64 md:h-64 lg:w-72 lg:h-72 rounded-full overflow-hidden shadow-lg animate-bounce">
                     <img src="{{ asset('background/bg.jpg') }}" alt="Gambar" class="w-full h-full object-cover">
                 </div>
             </div>
@@ -44,11 +44,11 @@
             const background = document.getElementById('background');
 
             background.addEventListener('mouseenter', function() {
-                background.classList.add('animate-bounce');
+                background.classList.remove('animate-bounce');
             });
 
             background.addEventListener('mouseleave', function() {
-                background.classList.remove('animate-bounce');
+                background.classList.add('animate-bounce');
             });
         });
     </script>
