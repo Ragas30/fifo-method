5d8e02d7deadb43c8c49e417e49031e97f60e652
Ragas30 <130697528+Ragas30@users.noreply.github.com>
2025-01-14 13:51:41 +0700

    change icon logo

--- resources/views/auth/loginPage.blade.php
+++ resources/views/auth/loginPage.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Login Page</title>
+    <link rel="icon" type="image/x-icon" href="{{ asset('logo/LOGO.png') }}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <style>
         .background {
@@ -23,14 +24,12 @@
             <div class="mt-4">
                 <label for="email" class="block text-white font-semibold">Email</label>
                 <input type="email" name="email" id="username"
-                    class="w-full p-2 mt-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#FF2D20]"
-                    required>
+                    class="w-full p-2 mt-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#FF2D20]" required>
             </div>
             <div class="mt-4">
                 <label for="password" class="block text-white font-semibold">Password</label>
                 <input type="password" name="password" id="password"
-                    class="w-full p-2 mt-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#FF2D20]"
-                    required>
+                    class="w-full p-2 mt-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#FF2D20]" required>
             </div>
             <button type="submit"
                 class="w-full bg-blue-300 text-white py-2 mt-4 rounded-lg hover:bg-blue-500 transition-colors duration-200 ease-in-out hover:scale-105">
@@ -50,4 +49,3 @@
 </body>
 
 </html>
-
--- resources/views/dashboard/layout.blade.php
+++ resources/views/dashboard/layout.blade.php
@@ -5,8 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="icon" type="image/x-icon"
-        href="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1200px-Vue.js_Logo_2.svg.png">
+    <link rel="icon" type="image/x-icon" href="{{ asset('logo/LOGO.png') }}">
     <title>@yield('title')</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
